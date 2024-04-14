c766da7db410b722ea00e509cd76d7eaf883fc47
dmitri <dmitriy.tsarkov@rt.ru>
2024-04-14 23:43:40 +0300

    HW1 rework

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,9 +11,9 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	var result strings.Builder
+	var prev rune
 	str := []rune(s)
 
-	var prev rune
 	for i, char := range str {
 		if unicode.IsDigit(char) && i == 0 {
 			return "", ErrInvalidString
@@ -22,29 +22,26 @@ func Unpack(s string) (string, error) {
 		if unicode.IsDigit(char) && unicode.IsDigit(str[i-1]) {
 			return "", ErrInvalidString
 		}
+
 		if unicode.IsDigit(char) {
 			digit, err := strconv.Atoi(string(char))
 			if err != nil {
 				return "", err
 			}
+
 			if digit == 0 {
-				// Пропускаем предыдущий символ
 				prev = 0
 			} else {
-				// Добавляем повторения предыдущего символа
 				if prev != 0 {
 					result.WriteString(strings.Repeat(string(prev), digit-1))
 				}
 			}
 		} else {
-			// Добавляем текущий символ в результат
 			if prev != 0 {
 				result.WriteString(string(prev))
 			}
 			prev = char
 		}
-
-		// Если это последний символ и он не цифра, добавляем его в результат
 		if i == len(str)-1 && !unicode.IsDigit(char) {
 			result.WriteString(string(char))
 		}
