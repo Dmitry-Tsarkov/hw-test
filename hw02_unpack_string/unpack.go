555ad0aaf225c8ebe81479abfc429837cd4aaff6
dmitri <dmitriy.tsarkov@rt.ru>
2024-04-15 18:42:39 +0300

    HW2 rework

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,29 +13,23 @@ func Unpack(s string) (string, error) {
 	var result strings.Builder
 	var prev rune
 	str := []rune(s)
-
 	for i, char := range str {
-		if unicode.IsDigit(char) && i == 0 {
+		if i == 0 && unicode.IsDigit(char) {
 			return "", ErrInvalidString
 		}
-
 		if unicode.IsDigit(char) && unicode.IsDigit(str[i-1]) {
 			return "", ErrInvalidString
 		}
-
 		if unicode.IsDigit(char) {
 			digit, err := strconv.Atoi(string(char))
 			if err != nil {
 				return "", err
 			}
-
 			if digit == 0 {
 				prev = 0
-			} else {
-				if prev != 0 {
-					result.WriteString(strings.Repeat(string(prev), digit-1))
-				}
+				continue
 			}
+			result.WriteString(strings.Repeat(string(prev), digit-1))
 		} else {
 			if prev != 0 {
 				result.WriteString(string(prev))
