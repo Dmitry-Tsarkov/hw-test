c18cc62551023a67e3080f5b203006b8d56b8390
dmitri <dmitriy.tsarkov@rt.ru>
2024-04-17 22:34:08 +0300

    HW2 added flag

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,6 +12,7 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(s string) (string, error) {
 	var result strings.Builder
 	var prev rune
+	var hasPrev bool
 	str := []rune(s)
 	for i, char := range str {
 		if i == 0 && unicode.IsDigit(char) {
@@ -26,14 +27,15 @@ func Unpack(s string) (string, error) {
 				return "", err
 			}
 			if digit == 0 {
-				prev = 0
+				hasPrev = false
 				continue
 			}
 			result.WriteString(strings.Repeat(string(prev), digit-1))
 		} else {
-			if prev != 0 {
+			if hasPrev {
 				result.WriteString(string(prev))
 			}
+			hasPrev = true
 			prev = char
 		}
 		if i == len(str)-1 && !unicode.IsDigit(char) {
