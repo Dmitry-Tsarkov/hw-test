bcbc56c089048fe2bc66d2bc372544521cedf7db
dmitri <dmitriy.tsarkov@rt.ru>
2024-06-24 21:38:45 +0300

    hw05_parallel_execution_without_asterisk

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,22 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+	t.Run("if maxErrorsCount is zero, all tasks should be executed", func(t *testing.T) {
+		tasksCount := 10
+		tasks := make([]Task, 0, tasksCount)
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				return nil
+			})
+		}
+
+		workersCount := 3
+		maxErrorsCount := 0
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Error(t, err)
+		require.Equal(t, ErrErrorsLimitExceeded, err, "expected ErrErrorsLimitExceeded when maxErrorsCount <= 0")
+	})
 }
